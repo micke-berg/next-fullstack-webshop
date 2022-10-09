8d7281f1b8c9dabf49b33ba0ef8e91b62d74f6ec
Micke Berg <micke.berg@gmail.com>
2022-10-09 18:51:46 +0200

    new product item component

---
+++ components/ProductItem.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+
+const ProductItem = ({ product }) => {
+	return (
+		<div className="card">
+			<Link href={`/products/${product.slug}`}>
+				<a>
+					<img
+						src={product.image}
+						alt={product.name}
+						className="rouned shadow"
+					/>
+				</a>
+			</Link>
+			<div className="flex flex-col items-center justify-center p-5">
+				<Link href={`/products/${product.slug}`}>
+					<a>
+						<h2 className="text-lg">{product.name}</h2>
+					</a>
+				</Link>
+				<p className="mb-2">{product.brand}</p>
+				<p>${product.price}</p>
+				<button type="button" className="primary-button">
+					Add to cart
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default ProductItem;
