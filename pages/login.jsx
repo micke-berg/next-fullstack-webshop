fa76019d8a4a60f1f20d8e7503c5f9ef758aa945
Micke Berg <micke.berg@gmail.com>
2022-11-02 14:01:11 +0100

    useSession, toast, refactor submitHandler

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,17 +1,44 @@
 import Link from 'next/link';
 import React from 'react';
+import { signIn, useSession } from 'next-auth/react';
 import { useForm } from 'react-hook-form';
 import Layout from '../components/Layout';
+import { getError } from '../utils/error';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const LoginScreen = () => {
+	const { data: session } = useSession();
+
+	const router = useRouter();
+	const { redirect } = router.query;
+
+	useEffect(() => {
+		if (session?.user) {
+			router.push(redirect || '/');
+		}
+	}, [redirect, router, session]);
+
 	const {
 		handleSubmit,
 		register,
 		formState: { errors },
 	} = useForm();
 
-	const submitHandler = ({ email, password }) => {
-		console.log(`${email} ${password}`);
+	const submitHandler = async ({ email, password }) => {
+		try {
+			const result = await signIn('credentials', {
+				redirect: false,
+				email,
+				password,
+			});
+			if (result.erreor) {
+				toast.error(result.error);
+			}
+		} catch (err) {
+			toast.error(getError(err));
+		}
 	};
 
 	return (
