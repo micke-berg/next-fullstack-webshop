206acd42736119efc0c88aa5060349497e4229d5
Micke Berg <micke.berg@gmail.com>
2022-11-02 15:46:40 +0100

    CART_RESET

--- utils/Store.js
+++ utils/Store.js
@@ -31,6 +31,16 @@ function reducer(state, action) {
 			Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }));
 			return { ...state, cart: { ...state.cart, cartItems } };
 		}
+		case 'CART_RESET': {
+			return {
+				...state,
+				cart: {
+					cartItems: [],
+					shippmentAdress: { location: {} },
+					paymentMethod: '',
+				},
+			};
+		}
 		default:
 			return state;
 	}
