0a113b08a8718eae6981a42a839a5bc66dacf501
Micke Berg <micke.berg@gmail.com>
2022-11-02 15:58:19 +0100

    fix spelling mistakes, and some import stuff

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
-import { CredentialsProvider } from 'next-auth/providers';
-import User from '../../../models/Users';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import User from '../../../models/User';
 import db from '../../../utils/db';
 import bcryptjs from 'bcryptjs';
 
@@ -10,23 +10,24 @@ export default NextAuth({
 	},
 	callbacks: {
 		async jwt({ token, user }) {
-			if (user?.id) token._id = user._id;
-			if (user?.isAdmin) token._isAdmin = user.isAdmin;
+			if (user?._id) token._id = user._id;
+			if (user?.isAdmin) token.isAdmin = user.isAdmin;
 			return token;
 		},
 		async session({ session, token }) {
-			if (token?.id) session.user._id = token._id;
+			if (token?._id) session.user._id = token._id;
 			if (token?.isAdmin) session.user.isAdmin = token.isAdmin;
+			return session;
 		},
 	},
 	providers: [
 		CredentialsProvider({
-			async autorize(credentials) {
+			async authorize(credentials) {
 				await db.connect();
 				const user = await User.findOne({
 					email: credentials.email,
 				});
-				await db.disconect();
+				await db.disconnect();
 				if (user && bcryptjs.compareSync(credentials.password, user.password)) {
 					return {
 						_id: user._id,
