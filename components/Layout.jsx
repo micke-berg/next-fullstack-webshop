0770ce58a22b0a6822e9b494d031cffac5a41ae5
Micke Berg <micke.berg@gmail.com>
2022-11-06 14:40:37 +0100

    header icons, styling, quantity

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,6 +7,7 @@ import { Menu } from '@headlessui/react';
 import { Store } from '../utils/Store';
 import DropdownLink from './DropdownLink';
 import Cookies from 'js-cookie';
+import { SlUser, SlBag } from 'react-icons/sl';
 
 const Layout = ({ children, title }) => {
 	const { status, data: session } = useSession();
@@ -15,16 +16,13 @@ const Layout = ({ children, title }) => {
 	const [cartItemsCount, setCartItemsCount] = useState(0);
 
 	useEffect(() => {
-		setCartItemsCount(
-			cart.cartItems.reduce((prev, current) => prev + current.quantity, 0)
-		);
+		setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
 	}, [cart.cartItems]);
 
 	const logutClickHandler = () => {
 		Cookies.remove('cart');
 		dispatch({ type: 'CART_RESET' });
 		signOut({ callbackUrl: '/login' });
-		console.log('hej');
 	};
 
 	return (
@@ -37,16 +35,16 @@ const Layout = ({ children, title }) => {
 			<ToastContainer possition limit={1} />
 			<div className="flex min-h-screen flex-col justify-between">
 				<header>
-					<nav className="flex h-12 px-4 justify-between items-center shadow-md">
+					<nav className="flex h-14 px-4 justify-between items-center bg-neutral-900">
 						<Link href="/">
-							<a className="text-lg font-bold">Webshop.</a>
+							<a className="text-lg font-bold text-white">Webshop.</a>
 						</Link>
-						<div>
+						<div className="flex align-middle">
 							<Link href="/cart">
-								<a className="p-2">
-									Cart
+								<a className="p-2 text-white relative">
+									<SlBag className="h-6 w-6 mr-1 absolute right-5 top-0" />
 									{cartItemsCount > 0 && (
-										<span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+										<span className="text-center rounded-full bg-white text-black w-5 h-5 text-xs absolute bottom-2 right-3 leading-5">
 											{cartItemsCount}
 										</span>
 									)}
@@ -56,11 +54,11 @@ const Layout = ({ children, title }) => {
 							{status === 'loading' ? (
 								'loading'
 							) : session?.user ? (
-								<Menu as="div" className="relative inline-block">
-									<Menu.Button className="text-blue-600">
+								<Menu as="div" className="relative inline-block z-10 ">
+									<Menu.Button className="text-white">
 										{session.user.name}
 									</Menu.Button>
-									<Menu.Items className="absolute right-0 w-56 origin-top-right bg-white shadow-lg">
+									<Menu.Items className="fixed  w-full md:absolute md:w-56 right-0 top-12 origin-top-right bg-neutral-900 text-white">
 										<Menu.Item>
 											<DropdownLink className="dropdown-link" href="/profile">
 												Profile
@@ -84,15 +82,17 @@ const Layout = ({ children, title }) => {
 								</Menu>
 							) : (
 								<Link href="/login">
-									<a className="p-2">Login</a>
+									<a>
+										<SlUser className="h-6 w-6 text-white" />
+									</a>
 								</Link>
 							)}
 						</div>
 					</nav>
 				</header>
 				<main className="container m-auto mt-4 px-4">{children}</main>
-				<footer className="flex justify-center items-center h-10 shadow-inner">
-					Copyright &copy; 2022 Webshop
+				<footer className="flex justify-center items-center h-12  mt-4 text-white bg-neutral-900 text-md font-thin">
+					Copyright &copy; 2022 Webshop.
 				</footer>
 			</div>
 		</>
