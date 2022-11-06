9131acbac145a245321c0d523ac72040a0fa65a4
Micke Berg <micke.berg@gmail.com>
2022-11-06 14:43:53 +0100

    move quantity, styling, icons

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useContext } from 'react';
-import { XCircleIcon, ArrowLeftIcon } from '@heroicons/react/outline';
+import { useContext, useEffect, useState } from 'react';
+import { SlArrowLeftCircle, SlClose } from 'react-icons/sl';
+import { TfiTrash } from 'react-icons/tfi';
+
 import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
 import { useRouter } from 'next/router';
@@ -9,12 +11,15 @@ import dynamic from 'next/dynamic';
 
 const CartScreen = () => {
 	const { state, dispatch } = useContext(Store);
-
 	const router = useRouter();
-
+	const [cartItemsCount, setCartItemsCount] = useState(0);
 	const {
 		cart: { cartItems },
 	} = state;
+
+	useEffect(() => {
+		setCartItemsCount(cartItems.reduce((a, c) => a + c.quantity, 0));
+	}, [cartItems]);
 
 	const removeItemHandler = (item) => {
 		dispatch({ type: 'CART_REMOVE_ITEM', payload: item });
@@ -27,24 +32,33 @@ const CartScreen = () => {
 
 	return (
 		<Layout title="Shopping Cart">
-			<Link href="/">
-				<a className="flex items-center">
-					<ArrowLeftIcon className="h-5 w-5 mr-1" />
-					Continue shopping
-				</a>
-			</Link>
-			<h1 className="mb-4 mt-2 text-xl">Shopping Cart</h1>
+			{cartItems.length !== 0 && (
+				<Link href="/">
+					<a className="flex items-center text-gray-500 text-sm">
+						<SlArrowLeftCircle className="h-5 w-5 mr-2" />
+						Continue shopping
+					</a>
+				</Link>
+			)}
+			<h1 className="mb-6 mt-2 text-xl border-b pb-4 pt-4">
+				Shopping Cart ({cartItemsCount})
+			</h1>
 			{cartItems.length === 0 ? (
-				<div>
-					Cart is empty! <Link href="/">Go shopping</Link>
+				<div className="flex flex-col">
+					Cart is empty!{' '}
+					<Link href="/">
+						<button className="black-button w-full md:w-1/4 mt-4">
+							<a>Go shopping</a>
+						</button>
+					</Link>
 				</div>
 			) : (
 				<div className="grid md:grid-cols-4 md:gap-5">
-					<div className="overflow-x-auto md:col-span-3">
+					<div className="overflow-x-auto md:col-span-3 mb-4">
 						<table className="min-w-full">
-							<thead className="border-b">
+							<thead className="border-b text-bold">
 								<tr>
-									<th className="px-5 text-left">Item</th>
+									<th className="pr-5 text-left">Item</th>
 									<th className="p-5 text-right">Quantity</th>
 									<th className="p-5 text-right">Price</th>
 									<th className="p-5 text-center">Action</th>
@@ -52,15 +66,15 @@ const CartScreen = () => {
 							</thead>
 							<tbody>
 								{cartItems.map((item) => (
-									<tr key={item.slug} className="border-b">
+									<tr key={item.slug} className="border-b text-gray-500">
 										<td>
 											<Link href={`/product/${item.slug}`}>
-												<a className="flex items-center">
+												<a className="flex items-center ">
 													<Image
 														src={item.image}
 														alt={item.name}
-														width={50}
-														height={50}
+														width={70}
+														height={70}
 													></Image>
 													&nbsp; &nbsp;
 													{item.name}
@@ -84,7 +98,8 @@ const CartScreen = () => {
 										<td className="p-5 text-right">$ {item.price}</td>
 										<td className="p-5 text-center">
 											<button onClick={() => removeItemHandler(item)}>
-												<XCircleIcon className="h-5 w-5"></XCircleIcon>
+												<TfiTrash className="h-5 w-5" />
+												{/* <SlClose className="h-5 w-5"></SlClose> */}
 											</button>
 										</td>
 									</tr>
@@ -92,16 +107,11 @@ const CartScreen = () => {
 							</tbody>
 						</table>
 					</div>
-					<div className="card p-5">
+					<div>
 						<ul>
 							<li>
-								<div className="pb-3 text-xl">
-									Subtotal (
-									{cartItems.reduce(
-										(prev, current) => prev + current.quantity,
-										0
-									)}
-									) : ${' '}
+								<div className="pb-3 text-gray-600">
+									Subtotal: ${' '}
 									{cartItems.reduce(
 										(prev, current) => prev + current.quantity * current.price,
 										0
@@ -111,9 +121,9 @@ const CartScreen = () => {
 							<li>
 								<button
 									onClick={() => router.push('login?redirect=/shipping')}
-									className="primary-button w-full"
+									className="black-button w-full"
 								>
-									Check Out
+									Check out{' '}
 								</button>
 							</li>
 						</ul>
