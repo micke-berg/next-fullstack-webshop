d6df3a3dabb9036b683cdde0955b784386319707
Micke Berg <micke.berg@gmail.com>
2022-11-06 14:41:43 +0100

    remove button and border

--- components/ProductItem.jsx
+++ components/ProductItem.jsx
@@ -3,27 +3,16 @@ import Link from 'next/link';
 
 const ProductItem = ({ product }) => {
 	return (
-		<div className="card">
+		<div>
 			<Link href={`/product/${product.slug}`}>
 				<a>
-					<img
-						src={product.image}
-						alt={product.name}
-						className="rouned shadow"
-					/>
+					<img src={product.image} alt={product.name} />
 				</a>
 			</Link>
-			<div className="flex flex-col items-center justify-center p-5">
-				<Link href={`/products/${product.slug}`}>
-					<a>
-						<h2 className="text-lg">{product.name}</h2>
-					</a>
-				</Link>
-				<p className="mb-2">{product.brand}</p>
-				<p>${product.price}</p>
-				<button type="button" className="primary-button">
-					Add to cart
-				</button>
+			<div className="flex flex-col items-center justify-center p-4 font-thin tracking-wide">
+				<h2 className="text-md font-normal">{product.name}</h2>
+				<p>{product.brand}</p>
+				<p className="mb-2">$ {product.price}</p>
 			</div>
 		</div>
 	);
