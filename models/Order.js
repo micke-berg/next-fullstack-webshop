e628574af41e51e96f6d5ba0e54a4d180161b776
Micke Berg <micke.berg@gmail.com>
2022-11-09 22:07:19 +0100

    change of types

--- models/Order.js
+++ models/Order.js
@@ -14,20 +14,18 @@ const orderSchema = new mongoose.Schema(
 		shippingAddress: {
 			fullName: { type: String, required: true },
 			address: { type: String, required: true },
-			city: { type: Number, required: true },
-			postalCode: { type: Number, required: true },
-			country: { type: Number, required: true },
+			city: { type: String, required: true },
+			postalCode: { type: String, required: true },
+			country: { type: String, required: true },
 		},
-		paymentMethod: {
-			type: String,
-			required: true,
-		},
+		paymentMethod: { type: String, required: true },
+		paymentResult: { id: String, status: String, email_address: String },
 		itemsPrice: { type: Number, required: true },
 		shippingPrice: { type: Number, required: true },
 		taxPrice: { type: Number, required: true },
 		totalPrice: { type: Number, required: true },
-		isPaid: { type: Boolean, required: true },
-		isDelivered: { type: Boolean, required: true },
+		isPaid: { type: Boolean, required: true, default: false },
+		isDelivered: { type: Boolean, required: true, default: false },
 		paidAt: { type: Date },
 		deliveredAt: { type: Date },
 	},
@@ -36,5 +34,5 @@ const orderSchema = new mongoose.Schema(
 	}
 );
 
-const Order = mongoose.models.User || mongoose.model('Order', orderSchema);
+const Order = mongoose.models.Order || mongoose.model('Order', orderSchema);
 export default Order;
