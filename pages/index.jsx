38fdec09de61c9d60903d2104eaf234a9c800983
Micke Berg <micke.berg@gmail.com>
2022-11-09 08:50:43 +0100

    getServerSideProps, import db connect

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,26 @@
 import Layout from '../components/Layout';
 import ProductItem from '../components/ProductItem';
-import data from '../utils/data';
+import Product from '../models/Product';
+import db from '../utils/db';
 
-export default function Home() {
+export default function Home({ products }) {
 	return (
 		<Layout>
 			<div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-				{data.products.map((product) => (
+				{products.map((product) => (
 					<ProductItem key={product.slug} product={product} />
 				))}
 			</div>
 		</Layout>
 	);
+}
+
+export async function getServerSideProps() {
+	await db.connect();
+	const products = await Product.find().lean();
+	return {
+		props: {
+			products: products.map(db.convertDocToObj),
+		},
+	};
 }
