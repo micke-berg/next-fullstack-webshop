6a2e9a6d5474f5284552e0eea7ccfb93ce0154a2
Micke Berg <micke.berg@gmail.com>
2022-11-12 14:18:52 +0100

    getSession

--- pages/api/admin/orders/[id]/deliver.js
+++ pages/api/admin/orders/[id]/deliver.js
@@ -1,7 +1,12 @@
+import { getSession } from 'next-auth/react';
 import Order from '../../../../../models/Order';
 import db from '../../../../../utils/db';
 
 const handler = async (req, res) => {
+	const session = await getSession({ req });
+	if (!session || (session && !session.user.isAdmin)) {
+		return res.status(402).send('Error: Signin required');
+	}
 	await db.connect();
 	const order = await Order.findById(req.query.id);
 	if (order) {
@@ -9,7 +14,7 @@ const handler = async (req, res) => {
 		order.deliveredAt = Date.now();
 		const deliverOrder = await order.save();
 		await db.disconnect();
-		res.send({ message: 'Order delivere successfully', order: deliverOrder });
+		res.send({ message: 'Order delivered successfully', order: deliverOrder });
 	} else {
 		db.disconnect();
 		res.status(404).send({ message: 'Error: order not found' });
